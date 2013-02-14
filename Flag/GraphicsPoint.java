40830c3ae66b4c3c98507891eecb984294668089
KLW <weistrofferkl@s.dcsdk12.org>
2013-02-14 08:10:55 -0700

    working GraphicsPoint

--- Flag/GraphicsPoint.java
+++ Flag/GraphicsPoint.java
@@ -4,10 +4,35 @@ public class GraphicsPoint{
 
 	private int x,y;
 
-	public Point(int x, int y){
-			this.x = x;
-			this.y = y;
+	public GraphicsPoint(int x, int y){
 
+
+			moveTo(x,y);
+		}
+
+		public void setX(int x){
+
+			if (x>= 0){
+			this.x = x;
+			}
+
+		}
+		public void getX(int x){
+			return this.x;
+		}
+		public void setY(int y){
+			if(y >= 0){
+			this.y = y;
+			}
+		}
+		public void getY(int y){
+			return this.y;
+		}
+
+
+		public void moveTo(int x, int y){
+			setY(y);
+			setX(x);
 		}
 
 }
